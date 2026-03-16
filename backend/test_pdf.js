ead7b2544952304bfd74c4ce6c56f7138a15aed8
Maurizio Blasilli <vivamau@Maurizios-Mac-mini.local>
2026-03-16 23:11:29 +0100

    some cleaning

--- backend/test_pdf.js
+++
@@ -1,9 +0,0 @@
-const { PDFParse } = require('pdf-parse');
-
-try {
-    const parser = new PDFParse();
-    console.log('Parser properties:', Object.getOwnPropertyNames(Object.getPrototypeOf(parser)));
-    console.log('Parser keys:', Object.keys(parser));
-} catch (err) {
-    console.error(err.message);
-}
